b1eb244c00992361f7dcbfa17dfdcd1cc25f5dd7
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2020-11-29 17:57:21 -0800

    added javascript

--- sourceJavascript.js
+++ sourceJavascript.js
@@ -2,3 +2,10 @@
 
 
 let container = document.getElementById("container");
+
+container.style.color = "orange";
+
+let firstDiv = document.getElementsByTagName("div")[0];
+
+firstDiv.style.color = "red";
+firstDiv.style.backgroundColor = "teal";

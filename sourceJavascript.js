aa4da86f6c0893f136972e9c6db14888961ca92d
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2020-11-29 18:37:00 -0800

    added button

--- sourceJavascript.js
+++ sourceJavascript.js
@@ -5,7 +5,15 @@ let container = document.getElementById("container");
 
 container.style.color = "orange";
 
+let body = document.getElementById("container");
+
 let firstDiv = document.getElementsByTagName("div")[0];
 
 firstDiv.style.color = "red";
 firstDiv.style.backgroundColor = "teal";
+
+let button = document.createElement("button");
+button.innerText = "I am a button created with Javascript!";
+
+container.appendChild(button);
+
